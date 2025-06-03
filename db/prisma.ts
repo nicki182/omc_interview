6f0f7db9618b8426fa4ee065a7e61682457e76d9
nicky <Nicky.stru@gmail.com>
2025-06-03 13:11:48 +0300

    fix: took out an unnecessary console.log

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "../generated/prisma";
-import { Face } from "../generated/prisma";
 export type {
   Temperature,
   Malfunction,
@@ -7,6 +6,5 @@ export type {
   AggregatedTemperature,
 } from "../generated/prisma";
 export { Time, Face } from "../generated/prisma";
-console.log(Face);
 const prisma = new PrismaClient();
 export default prisma;
